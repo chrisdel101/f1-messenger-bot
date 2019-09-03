cce16b4fcc4138516c0f1b7dea615286b3f775e1
Chris Del <drumgod101@gmail.com>
2019-09-02 21:58:58 -0600

    fix

--- controllers/webhook.controller.js
+++ controllers/webhook.controller.js
@@ -19,7 +19,7 @@ exports.sendHookResponse = (req, res) => {
       // Check if the event is a message or postback and
       // pass the event to the appropriate handler function
       if (webhook_event.message) {
-        handleMessage(sender_psid, webhook_event)
+        module.exports.handleMessage(sender_psid, webhook_event)
       } else if (webhook_event.postback) {
         handlePostback(sender_psid, webhook_event.postback)
       }
