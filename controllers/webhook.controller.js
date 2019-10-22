2b68322593f1afb82a4e881b50b63130712c3a4c
Chris Del <drumgod101@gmail.com>
2019-10-21 23:10:22 -0600

    add log

--- controllers/webhook.controller.js
+++ controllers/webhook.controller.js
@@ -27,34 +27,35 @@ exports.getUserData = () => {
 // returns array
 exports.sendHookResponse = (req, res) => {
   console.log('HERLLO')
-  let body = req.body
-  // Checks this is an event from a page subscription
+  // let body = req.body
+  // // Checks this is an event from a page subscription
   if (body.object === 'page') {
-    // Iterates over each entry - there may be multiple if batched
-    return body.entry.map(function(entry) {
-      // Gets the message. entry.messaging is an array, but
-      // will only ever contain one message, so we get index 0
-      let webhook_event = entry.messaging[0]
-      // console.log('W', webhook_event)
-      // Get the sender PSID
-      let sender_psid = webhook_event.sender.id
-      console.log('Sender PSID: ' + sender_psid)
-      // Check if the event is a message or postback and
-      // pass the event to the appropriate handler function
-      if (webhook_event.message) {
-        // get devivce size and data
-        const cardType = module.exports.getUserData()
-        // Returns a '200 OK' response to all requests
-        res.status(200).send('EVENT_RECEIVED')
-        return module.exports.handleMessageType(
-          sender_psid,
-          webhook_event,
-          cardType
-        )
-      } else if (webhook_event.postback) {
-        handlePostback(sender_psid, webhook_event.postback)
-      }
-    })
+    //   // Iterates over each entry - there may be multiple if batched
+    //   return body.entry.map(function(entry) {
+    //     // Gets the message. entry.messaging is an array, but
+    //     // will only ever contain one message, so we get index 0
+    //     let webhook_event = entry.messaging[0]
+    //     // console.log('W', webhook_event)
+    //     // Get the sender PSID
+    //     let sender_psid = webhook_event.sender.id
+    //     console.log('Sender PSID: ' + sender_psid)
+    //     // Check if the event is a message or postback and
+    //     // pass the event to the appropriate handler function
+    //     if (webhook_event.message) {
+    //       // get devivce size and data
+    //       const cardType = module.exports.getUserData()
+    //       // Returns a '200 OK' response to all requests
+    //       res.status(200).send('EVENT_RECEIVED')
+    //       return module.exports.handleMessageType(
+    //         sender_psid,
+    //         webhook_event,
+    //         cardType
+    //       )
+    //     } else if (webhook_event.postback) {
+    //       handlePostback(sender_psid, webhook_event.postback)
+    //     }
+    //   })
+    res.status(200).send('EVENT_RECEIVED')
   } else {
     // Returns a '404 Not Found' if event is not from a page subscription
     return res.sendStatus(404)
